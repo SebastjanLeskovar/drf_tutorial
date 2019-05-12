7822cc876c59f5d07ed5db8f6dbd0571a408cec8
Sebastjan Leskovar <sebastjan.leskovar@gmail.com>
2019-05-12 17:09:22 +0200

    Tutorial 7

--- snippets/views.py
+++ snippets/views.py
@@ -1,15 +1,10 @@
 from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import generics
-from rest_framework import permissions
 
-from rest_framework.response import Response
-from rest_framework import renderers
-from rest_framework import viewsets
+from rest_framework import permissions, renderers, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
 from snippets.serializers import UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
 from django.urls import path
 
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
+
+
 urlpatterns = [
+    path('schema/', schema_view),
     path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
